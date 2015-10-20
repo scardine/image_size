692e61ff6ec1461f8083f5a9a654292f080a08f4
Wes Turner <wes@wrd.nu>
2015-10-20 04:58:01 -0500

    BLD: setup.py: read_long_description(*), get-image-size entry_point

--- setup.py
+++ setup.py
@@ -2,17 +2,27 @@ from setuptools import setup
 
 VERSION = '0.2.0'
 
+def read_long_description():
+    long_desc = []
+    with codecs.open('README.rst', 'r', 'utf8') as longdesc:
+        long_desc.append(longdesc.read())
+    with codecs.open('HISTORY.rst', 'r', 'utf8') as history:
+        long_desc.append(history.read())
+    return u'\n\n'.join(long_desc)
+
+LONG_DESCRIPTION = read_long_description()
+
 setup(
     name='get_image_size',
     url='https://github.com/scardine/image_size',
     version=VERSION,
-    long_description=open('README.rst').read(),
+    long_description=LONG_DESCRIPTION,
     author='github.com/scardine',
     author_email=' ',
     py_modules=['get_image_size'],
     entry_points={
       'console_scripts': [
-        'get_image_size = get_image_size:main',
+        'get-image-size = get_image_size:main',
         ],
     },
 )
